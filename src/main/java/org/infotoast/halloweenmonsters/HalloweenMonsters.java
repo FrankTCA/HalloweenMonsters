1e27e101e0c8b7407db6a227623c75911ccdb6b7
Frank <frank@steamedveggie.com>
2024-08-05 15:35:23 -0400

    Initial commit

---
+++ src/main/java/org/infotoast/halloweenmonsters/HalloweenMonsters.java
@@ -0,0 +1,17 @@
+package org.infotoast.halloweenmonsters;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HalloweenMonsters extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
