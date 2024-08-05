925ca844d16a1ee1b870164d2d38ad95af66bee1
Frank <frank@steamedveggie.com>
2024-08-05 16:37:35 -0400

    Initial commit

--- src/main/java/org/infotoast/halloweenmonsters/HalloweenMonsters.java
+++ src/main/java/org/infotoast/halloweenmonsters/HalloweenMonsters.java
@@ -1,17 +1,39 @@
 package org.infotoast.halloweenmonsters;
 
+import me.casperge.realisticseasons.api.SeasonsAPI;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class HalloweenMonsters extends JavaPlugin {
+import java.util.List;
+
+public final class HalloweenMonsters extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getLogger().info("HalloweenMonsters is enabled");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onEntitySpawn(EntitySpawnEvent evt) {
+        if (getConfig().getStringList("Prevent-Spawn").contains(evt.getEntity().getType().toString().toLowerCase())) {
+            SeasonsAPI seasons = SeasonsAPI.getInstance();
+            List<String> activeEvents = seasons.getActiveEvents(evt.getLocation().getWorld());
+            if (activeEvents.contains("&6Halloween")) {
+                if (getConfig().getStringList("Halloween-Exceptions").contains(evt.getEntity().getType().toString().toLowerCase())) {
+                    return;
+                }
+            }
+            evt.setCancelled(true);
+        }
     }
 }
